a368ccce4c7ad0f91bb2a20f70dffd17ad5dbfd5
anayet71 <ariyananayet@gmail.com>
2025-02-11 20:45:32 +0600

    audio package added

--- src/Components/Phones/Phones.jsx
+++ src/Components/Phones/Phones.jsx
@@ -1,10 +1,13 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { BarChart, Bar, ResponsiveContainer, XAxis, YAxis } from 'recharts';
+import { Audio } from 'react-loader-spinner'
+
 
 
 const Phones = () => {
     const [phones, setPhones] = useState([])
+    const [loading, setLoading] = useState(true)
 
     useEffect(() => {
         // fetch("https://openapi.programming-hero.com/api/phones?search=iphone")
@@ -21,15 +24,25 @@ const Phones = () => {
                     return obj
                 })
                 console.log(phonesWithFakeData)
-                setPhones(phonesWithFakeData)
+                setPhones(phonesWithFakeData);
+                setLoading(false)
             })
     }, [])
     return (
         <div>
+           { loading && <Audio
+                height="80"
+                width="80"
+                radius="9"
+                color="green"
+                ariaLabel="three-dots-loading"
+                wrapperStyle
+                wrapperClass
+            />}
             <h2 className="text-5xl">Phones: {phones.length}</h2>
             <BarChart width={1000} height={400} data={phones}>
-            <XAxis dataKey="name" />
-            <YAxis></YAxis>
+                <XAxis dataKey="name" />
+                <YAxis></YAxis>
                 <Bar dataKey="price" fill="#ebebeb" />
             </BarChart>
         </div>
