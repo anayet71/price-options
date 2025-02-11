a7123d27929303893bb64b5c43a8e0628494b0a6
anayet71 <ariyananayet@gmail.com>
2025-02-11 13:32:48 +0600

    linechart data added

---
+++ src/Components/LineChart/LineChart.jsx
@@ -0,0 +1,23 @@
+
+const LineChart = () => {
+
+    const data = [
+        { id: 1, name: "Alice", marks: 78 },
+        { id: 2, name: "Bob", marks: 85 },
+        { id: 3, name: "Charlie", marks: 92 },
+        { id: 4, name: "David", marks: 88 },
+        { id: 5, name: "Eve", marks: 76 },
+        { id: 6, name: "Frank", marks: 95 },
+        { id: 7, name: "Grace", marks: 89 },
+        { id: 8, name: "Hannah", marks: 84 },
+        { id: 9, name: "Ivy", marks: 91 },
+        { id: 10, name: "Jack", marks: 87 },
+      ]; 
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default LineChart;
